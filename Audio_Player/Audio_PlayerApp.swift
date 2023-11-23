a8ea15f5a7ce93f9deb1730468d45bce8b2105ef
Mikhail Demichev <demichev.mi@gmail.com>
2023-11-23 18:29:43 +0700

    Initial Commit

---
+++ Audio_Player/Audio_PlayerApp.swift
@@ -0,0 +1,17 @@
+//
+//  Audio_PlayerApp.swift
+//  Audio_Player
+//
+//  Created by Mikhail Demichev on 23.11.2023.
+//
+
+import SwiftUI
+
+@main
+struct Audio_PlayerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
